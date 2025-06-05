11ef0e2ab28edc64d2a43b3cdb265e90e4bec655
MaorTe <maor884@gmail.com>
2025-06-05 20:26:46 +0300

    Production support using onrender

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5204/api",
+  baseURL: `${API_BASE}/api`,
   headers: {
     "Content-Type": "application/json",
   },
